899585e0596fceb772e76a5bc1e315e55ee773fb
mshahid153 <mdshahid150303@gmail.com>
2024-01-12 14:31:39 +0000

    Initial Commit

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      className="profile-img"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+    />
+    <h1 className="user-name">Shahid M</h1>
+    <p className="user-designation">Front-end Developer</p>
+  </div>
+)
+
+export default UserInfo
